7341c5a77e6b72e7d594d4e2f5eb18b0e8d3789b
fromscode <chakrabortyshubham00@gmail.com>
2025-05-04 07:28:59 +0000

    feat: create Edge class

---
+++ src/com/rmsca/Edge.java
@@ -0,0 +1,48 @@
+package com.rmsca;
+
+public class Edge {
+    private final static int MAX_SLOTS = 320;
+    private String from;
+    private String to;
+    private int weight;
+    private boolean[] spectrum;
+
+    public Edge(String from, String to, int weight) {
+        this.from = from;
+        this.to = to;
+        this.weight = weight;
+        this.spectrum = new boolean[MAX_SLOTS];
+    }
+
+    public String getFrom() {
+        return from;
+    }
+
+    public void setFrom(String from) {
+        this.from = from;
+    }
+
+    public String getTo() {
+        return to;
+    }
+
+    public void setTo(String to) {
+        this.to = to;
+    }
+
+    public int getWeight() {
+        return weight;
+    }
+
+    public void setWeight(int weight) {
+        this.weight = weight;
+    }
+
+    public boolean[] getSpectrum() {
+        return spectrum;
+    }
+
+    public void setSpectrum(boolean[] spectrum) {
+        this.spectrum = spectrum;
+    }    
+}
