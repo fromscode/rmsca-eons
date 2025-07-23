a8526c0c85575fab1524409a0a496573928f9a86
fromscode <chakrabortyshubham00@gmail.com>
2025-07-23 08:26:59 +0000

    feat: add static getter to expose NUM_CORES

--- src/com/rmsca/Node.java
+++ src/com/rmsca/Node.java
@@ -1,7 +1,7 @@
 package com.rmsca;
 
 public class Node {
-    private static final int NUMCORES = 3;
+    private static final int NUM_CORES = 3;
     private String name;
 
     public Node(String name) {
@@ -10,5 +10,9 @@ public class Node {
 
     public String toString() {
         return this.name;
+    }
+
+    public static int getNUM_CORES() {
+        return NUM_CORES;
     }
 }
