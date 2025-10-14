b8b483967d9243258532eea6dadff667de50ceed
Shubham Chakraborty <chakrabortyshubham00@gmail.com>
2025-10-14 06:20:05 +0000

    Add currCore details inside Node.java

--- src/com/rmsca/Node.java
+++ src/com/rmsca/Node.java
@@ -3,9 +3,19 @@ package com.rmsca;
 public class Node {
     private static final int NUM_CORES = 3;
     private String name;
+    private int currCore;
 
     public Node(String name) {
         this.name = name;
+        this.currCore = 0;
+    }
+
+    public int getCurrCore() {
+        return this.currCore;
+    }
+
+    public void setCurrCore() {
+        this.currCore = (this.currCore + 1) % NUM_CORES;
     }
 
     public String toString() {
